d36c78360b830ac82db69e77116c3c3c282aebce
Quang Vien <vienngocquang@gmail.com>
2025-10-20 20:00:54 +0200

    First Java Application

---
+++ My FirstApp/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /* This is the first comment line
+       This is the second comment line
+       This is the third comment line */
+    public static void main(String[] args) {
+	    // standalone line comment
+        System.out.println("First line from application"); // This is a line Comment
+        /* System.out.println("Second line from application"); */
+        System.out.println(/*Third line from application*/ " This is a different text ");
+    }
+}
