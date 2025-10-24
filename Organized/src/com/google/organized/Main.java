b7a965b3482687535452fe28fe6cbbf4f3976db1
Quang Vien <vienngocquang@gmail.com>
2025-10-24 23:05:16 +0200

    variable - primitive data types

--- Organized/src/com/google/organized/Main.java
+++ Organized/src/com/google/organized/Main.java
@@ -3,8 +3,8 @@ package com.google.organized;
 public class Main {
 
     public static void main(String[] args) {
-        byte a = 5;
-        System.out.println(a);
+        short b = 1024;
+        System.out.println(b);
     }
 
 }
