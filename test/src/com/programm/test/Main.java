12c1baaa980dea96ca1063361ca70e2450e6574f
longvien <vienngoclongvn@gmail.com>
2025-12-03 22:26:15 +0100

    revision

--- test/src/com/programm/test/Main.java
+++ test/src/com/programm/test/Main.java
@@ -3,13 +3,8 @@ package com.programm.test;
 public class Main {
 
     public static void main(String[] args) {
-	    var val1 = 5;
-        var val2 = 7;
-        if (val1 > val2)
-            System.out.println("val1 > val2");
-        else if (val1 < val2)
-            System.out.println("val1 < val2");
-        else
-            System.out.println("val 1 = val2");
+        varMethod.var();
+        float[] a = array.main();
+        System.out.println(a[0]);
     }
 }
---
+++ test/src/com/programm/test/array.java
@@ -0,0 +1,10 @@
+package com.programm.test;
+
+public class array {
+    static float[] main(){
+        float [] arrays = new float[3];
+        arrays[0] = 2.0f;
+        arrays[1] = 3.0f;
+        return arrays;
+    }
+}
