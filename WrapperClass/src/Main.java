d9af15ac06ee6eee495b31a1ed185c7c2c163a49
longvien <vienngoclongvn@gmail.com>
2025-12-28 16:27:53 +0100

    add wrapper class

---
+++ WrapperClass/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int valA = 10;
+        Integer valB = 50;
+        Integer valC = valA;
+        System.out.println(valB);
+
+    }
+}
